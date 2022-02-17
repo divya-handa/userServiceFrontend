4112656842dbffdda17552792aec2a0114c439ee
Divya Gupta <divya.gupta02@nagarro.com>
2022-02-17 17:19:48 +0530

    updated Ream me

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # userService
+
+API:
+"/" : get all users
+
+TO access rest api in backend use defined cluster ip: http://backend-clusterip-service/
--- src/main/java/com/nagp/devops/user/controllers/UserController.java
+++ src/main/java/com/nagp/devops/user/controllers/UserController.java
@@ -1,26 +1,17 @@
 package com.nagp.devops.user.controllers;
 
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Objects;
-
-import javax.annotation.Resource;
-
 import com.nagp.devops.user.entities.User;
-import com.nagp.devops.user.entities.UserKey;
 import com.nagp.devops.user.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/")
